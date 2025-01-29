4b6c437b31761fda5d49200c999d8d6390df4e5c
Vinícius Junqueira <vjovinicius@protonmail.com>
2025-01-28 21:02:21 -0300

    Ajustei o caminho dos arquivos estáticos

--- audiometria/settings.py
+++ audiometria/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+STATIC_ROOT = BASE_DIR / 'static'  # os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [BASE_DIR / 'templates/static']
 
 MESSAGE_TAGS = {
     constants.DEBUG: 'alert-info',
